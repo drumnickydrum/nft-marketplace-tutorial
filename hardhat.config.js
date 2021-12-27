a01ce2fae417bd4f34a254a05ef276124643128a
Nicky <nicky@pop-os.localdomain>
2021-12-27 13:37:23 -0500

    nft contracts

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require('@nomiclabs/hardhat-waffle');
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
+const projectId = '2ffc55cff91842e18a32148ba531d327';
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: '0.8.4',
+};
